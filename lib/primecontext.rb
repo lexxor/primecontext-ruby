8c2340d67939e18e00216e021926c1558505aec3
Alexey lexxor Korchagin <lexx-kor@ya.ru>
2014-08-27 00:56:30 +0700

    api

---
+++ lib/primecontext.rb
@@ -0,0 +1,14 @@
+require 'uri'
+require 'net/http'
+
+module Primecontext
+
+  def self.default_options
+    {
+      api_url: 'https://beta.primecontext.ru/api/crm/new-record/',
+      user_id: 0,
+      secret_key: '00000000000000000000000000000000'
+    }
+  end
+
+end
---
+++ lib/primecontext/api.rb
@@ -0,0 +1,30 @@
+module Primecontext
+
+  class Api
+    attr_reader :options
+
+    def initialize(options = {})
+      @options = Primecontext::default_options.merge(options)
+    end
+
+    def new_record(data = {})
+      data.merge!({ 'sign' => generate_sign(data) })
+      send_request(data)
+    end
+
+    def generate_sign(data)
+      data_str = data.sort.join.downcase
+      digest = OpenSSL::Digest.new 'sha256'
+      OpenSSL::HMAC.hexdigest digest, @options[:secret_key], data_str
+    end
+
+    def send_request(data)
+      uri = URI.parse(@options[:api_url])
+      http = Net::HTTP.new(uri.host, uri.port)
+      http.use_ssl = true
+      http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+      http.request_post(uri.request_uri, data.to_param)
+    end
+  end
+
+end
